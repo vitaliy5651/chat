c3f0159d0af12e8d6cef582d84f0ddde80262039
группы стационара <st@itstep.lan>
2021-01-13 21:55:17 +0300

    13.01.2021

--- scripts.js
+++ scripts.js
@@ -199,3 +199,15 @@ document.addEventListener('DOMContentLoaded', function() {
         // friendsListContainer.insertAdjacentHTML('beforeEnd', item);
     }
 });
+
+
+function ololo(str){
+ let count = 0;
+ const arr = ['a','o','e','i','u'];
+ for( let i of str){
+     if(arr.includes(i)){
+         count++;
+     }
+ }
+       return count;
+}
